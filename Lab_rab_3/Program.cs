1d95950ad259cfbe05bb2e75865cc5c2f660331f
dns <dns@LAPTOP-EHRCLL3N>
2023-11-18 21:16:21 +0200

    Добавьте файлы проекта.

---
+++ Lab_rab_3/Program.cs
@@ -0,0 +1,44 @@
+﻿//// Лабораторная работа 3. Задание 3
+//try
+//{
+//    Console.Write("Введите x: ");
+//    double x = double.Parse(Console.ReadLine());
+//    Console.WriteLine("Введите количество членов суммы ряда:");
+//    int n = int.Parse(Console.ReadLine());
+//    double sum = 0;
+//    double i = 1;
+//    int counter = 0;
+//    while (counter < n)
+//    {
+//        sum = (sum + ((Math.Sin((x * i))) / i));
+//        i = i + 2;
+//        counter++;
+//    }
+//    Console.WriteLine($"Сумма {n} членов ряда равна {sum:F2}.");
+//}
+//catch (Exception ex)
+//{ Console.WriteLine(ex.Message); }
+
+
+
+
+
+
+//Console.Write("Введите количество элементов массива: ");
+//int n = int.Parse(Console.ReadLine());
+//string[] mas = new string[n];
+//for (int i = 0; i < n; i++)
+//{
+
+//    for (int j = 1; j < n; j++)
+//    {
+//        if (mas[i] == mas[j])
+//        { Console.WriteLine($"Одинаковвые элементы массива на позициях с номерами {i} {j}");
+
+//        }
+
+//    }
+
+//}
+
+
