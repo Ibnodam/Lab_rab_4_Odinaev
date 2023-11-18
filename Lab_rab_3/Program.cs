1a4af6eba8d6cbb342091a9c94f5f7cc720232cf
Ibnodam <144240270+Ibnodam@users.noreply.github.com>
2023-11-18 21:28:21 +0200

    Update Program.cs

--- Lab_rab_3/Program.cs
+++ Lab_rab_3/Program.cs
@@ -1,21 +1,33 @@
 // Лабораторная работа 4. Задание 1.
+
 //Console.Write("Введите количество элементов массива: ");
 //int n = int.Parse(Console.ReadLine());
 //string[] mas = new string[n];
+
+
 //for (int i = 0; i < n; i++)
 //{
+//    Console.Write($"Введите элемент массива с номером {i}: ");
+//    mas[i] = Console.ReadLine();
+    
+//}
 
-//    for (int j = 1; j < n; j++)
+
+//for (int i = 0; i < n; i++)
+//{
+   
+
+//    for (int j = i + 1; j < n; j++)
 //    {
 //        if (mas[i] == mas[j])
-//        { Console.WriteLine($"Одинаковвые элементы массива на позициях с номерами {i} {j}");
+//        {
+//            Console.WriteLine($"Одинаковвые элементы массива на позициях с номерами {i} {j}: '{mas[i]}' и '{mas[j]}' соответственно.");
 
 //        }
 
 //    }
 
 //}
-
 
 
 
