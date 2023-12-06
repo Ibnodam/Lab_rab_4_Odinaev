17dcea024778b4d4e5af82598fc896e80cc052a7
Ibnodam <144240270+Ibnodam@users.noreply.github.com>
2023-12-06 22:16:08 +0200

    Update Program.cs

--- Lab_rab_3/Program.cs
+++ Lab_rab_3/Program.cs
@@ -31,3 +31,32 @@
 
 
 
+
+///////////////////////// Задание 2
+//////double max = 0;
+//////double[,] A = new double[5,10];
+//////Random random = new Random();
+//////Console.WriteLine("Исходная матрица A:");
+//////for (int i = 0; i < A.GetLength(0); i++)
+//////{
+//////    for (int j = 0; j < A.GetLength(1); j++)
+//////    {
+//////        A[i, j] = random.Next(-256, 256);
+//////        Console.Write(A[i, j] + "  " + "| ");
+//////        if (Math.Abs(A[i, j]) > max) max = A[i, j];
+//////    }
+//////    Console.WriteLine();
+//////}
+//////double[,] B = new double[5,10];
+//////Console.WriteLine($"Конечная матрица (число с максимальным модулем = {max}): ");
+//////for (int i = 0; i < B.GetLength(0); i++)
+//////{
+//////    for (int j = 0; j < B.GetLength(1); j++)
+//////    {
+//////        B[i, j] = A[i, j] / max;
+//////        Console.Write(Math.Round(B[i, j],2) + "  " + "| ");
+//////    }
+//////    Console.WriteLine();
+//////}
+
+
